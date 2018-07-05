7e2349d8359bc0012fa2f898a1b742cc314b9159
Alim Nastaev <alimnastaev@eltorolaptop45.eltoro.com>
2018-07-05 17:14:32 -0400

    first upload. Initial setup

---
+++ lib/community_web/schema.ex
@@ -0,0 +1,19 @@
+defmodule CommunityWeb.Schema do
+  use Absinthe.Schema
+
+  alias CommunityWeb.NewsResolver
+
+  object :link do
+    field :id, non_null(:id)
+    field :url, non_null(:string)
+    field :description, non_null(:string)
+  end
+
+  query do
+    # this is the query entry point to our app
+    field :all_links, non_null(list_of(non_null(:link))) do
+        resolve &NewsResolver.all_links/3
+    end
+  end
+  
+end
